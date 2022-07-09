35683a80af1eef100c034bc1440f1ff708fd5c75
Dariu Deszi <dariu.deszi@devtelsoft.onmicrosoft.com>
2022-07-09 13:57:31 +0300

    end of section 8

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,8 +22,6 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error =>{ console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
