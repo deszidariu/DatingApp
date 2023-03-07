8c9622410ff6142892ff35a0346ed79b04bd9971
dariu <dariu.deszi@devtelsoftware.com>
2023-03-07 16:33:31 +0200

    add sslmode

--- API/Program.cs
+++ API/Program.cs
@@ -34,7 +34,7 @@ else
         var pgHost = pgHostPort.Split(":")[0];
         var pgPort = pgHostPort.Split(":")[1];
 
-        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true;";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
